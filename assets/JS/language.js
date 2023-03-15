17f506e5f3ffca2a501e4d5b2771bf2fb53d8800
morteza <mortezashoeibi77@gmail.com>
2023-03-15 15:48:24 +0330

    add language changing system

--- assets/JS/language.js
+++ assets/JS/language.js
@@ -1,1 +1,12 @@
 document.getElementById("fa").style.display = "none";
+
+function en() {
+    document.getElementById("fa").style.display = "none";
+    document.getElementById("en").style.display = "block";
+}
+
+function fa() {
+    document.getElementById("en").style.display = "none";
+    document.getElementById("fa").style.display = "block";
+}
+
