7e1cef4515b23cccc2f97e3873f8c9d74dd44177
morteza <mortezashoeibi77@gmail.com>
2024-06-10 20:18:50 +0330

    delete all previous codes in order to do a rebuild

--- assets/JS/language.js
+++ assets/JS/language.js
@@ -3,10 +3,11 @@ document.getElementById("fa").style.display = "none";
 function en() {
     document.getElementById("fa").style.display = "none";
     document.getElementById("en").style.display = "block";
+    console.log("En");
 }
 
 function fa() {
     document.getElementById("en").style.display = "none";
     document.getElementById("fa").style.display = "block";
+    console.log("Fa");
 }
-
